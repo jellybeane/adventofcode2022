08ff4e9bed71cac9723cd7fef8a1b5e1fe5e93ee
jellybeane <elly.shao.94@gmail.com>
2022-12-15 23:10:43 -0800

    day 1 in progress

--- src/day16.rs
+++ src/day16.rs
@@ -1,21 +1,15 @@
-use std::{vec, collections::HashMap};
+use std::{vec, collections::HashMap, cmp::max};
 
-use petgraph::{graph::UnGraph, IntoWeightedEdge, Graph, Undirected, prelude::NodeIndex};
+use petgraph::{Graph, Undirected, prelude::NodeIndex};
 
 use aoc_runner_derive::{aoc, aoc_generator};
 
 use anyhow::Result;
 
-pub struct Valve {
-    index: u32,
-    name: String,
-    flow_rate: u32
-}
-
-// the map of valve names to flow rates,
-// map of valve names to graph indices,
+// the map of valve indices to flow rates,
+// map of valve names to valve indices,
 // and the graph of tunnel connections
-type Data = (HashMap<String,u32>,
+type Data = (HashMap<NodeIndex,u32>,
              HashMap<String,NodeIndex>,
             Graph<u32, u32, Undirected>);
 
@@ -42,9 +36,9 @@ fn input_generator_inner(input: &str) -> Result<Data> {
         let (_, valve_str) = valve_str.split_once(" ").unwrap();
         let valve_str = valve_str.to_string();
         let flow_rate:u32 = flow_rate_str.parse()?;
-        rate_map.insert(valve_str.clone(), flow_rate);
 
         let node_index = graph.add_node(flow_rate);
+        rate_map.insert(node_index, flow_rate);
         valve_name_map.insert(valve_str.clone(), node_index);
 
         // tunnels lead to valves DD, II, BB
@@ -71,24 +65,83 @@ fn input_generator_inner(input: &str) -> Result<Data> {
     Ok((rate_map, valve_name_map, graph))
 }
 
+// brute force DFS: it's too late for smarts
+// returns the highest pressure it saw in paths of length 30
+fn dfs(g: &Graph<u32, u32, Undirected>, 
+    cur_node:NodeIndex, cur_path:&mut Vec<NodeIndex>, 
+    indices_to_pressures:&HashMap<NodeIndex,u32>, total_pressure:u32, opened_on_prev_step:bool) // this feels bad ._.
+    -> u32
+{
+    // did we open a valve last node?
+    let mut updated_pressure = total_pressure;
+    if opened_on_prev_step {
+        let prev_node = cur_path.last().unwrap();
+        // EDIT: I have misunderstood the question
+        // We are given flow rates
+        // Pressures are flow rates * time the valves are open
+        // It's even worse than I thought o_o
+        updated_pressure += indices_to_pressures.get(prev_node).unwrap();
+    }
+
+    // is this the first time we encounter this valve?
+    let opened_this_step = !cur_path.contains(&cur_node);
+
+    // update the path
+    cur_path.push(cur_node);
+    // want total pressure by minute 30
+    if cur_path.len() >= 30 {
+        assert!(cur_path.len() == 30);//should never get past 30???
+        cur_path.pop();
+        return updated_pressure;
+    }
+
+    // the max if all valves are open
+    let max_possible: u32 = indices_to_pressures.values().sum();
+    assert!(updated_pressure <= max_possible);
+
+    // continue down the path
+    let mut max_seen = updated_pressure;
+    for neighbor in g.neighbors(cur_node)
+    {
+        let p = dfs(g, neighbor, cur_path, 
+            indices_to_pressures, updated_pressure, opened_this_step);
+        assert!(updated_pressure <= max_possible);  
+        max_seen = max(max_seen, p);
+        if max_seen == max_possible {
+            dbg!(max_seen, max_possible);
+            break;
+        }
+    }
+ 
+    // reset this node before returning
+    cur_path.pop();
+
+    max_seen
+}
+
 // Takes 1 minute to move through a tunnel
 // Can only open valves you are at
 // What is the most pressure you can release in 30 minutes?
 #[aoc(day16, part1)]
-pub fn solve_part1(input: &Data) -> usize {
+pub fn solve_part1(input: &Data) -> u32 {
     solve_part1_inner(input)
 }
-fn solve_part1_inner(input: &Data) -> usize {
+fn solve_part1_inner(input: &Data) -> u32 {
     let (rate_map, valve_name_map, tunnels) = input.clone();
 
-    unimplemented!()
+    dbg!(&rate_map);
+
+    // start at valve AA
+    let &cur_node = valve_name_map.get("AA").unwrap();
+    let mut cur_path = vec![];
+    dfs(&tunnels, cur_node, &mut cur_path, &rate_map, 0, false)
 }
 
 #[aoc(day16, part2)]
-pub fn solve_part2(input: &Data) -> usize {
+pub fn solve_part2(input: &Data) -> u32 {
     solve_part2_inner(input)
 }
-fn solve_part2_inner(input: &Data) -> usize {
+fn solve_part2_inner(input: &Data) -> u32 {
     unimplemented!()
 }
 
