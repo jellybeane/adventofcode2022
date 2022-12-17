7428c5c3dbd92e86d82a57903f86349b4a2cb12a
jellybeane <elly.shao.94@gmail.com>
2022-12-16 22:10:27 -0800

    zero-rate valves can be replaced by longer tunnels

--- src/day16.rs
+++ src/day16.rs
@@ -11,7 +11,7 @@ use anyhow::Result;
 // and the graph of tunnel connections
 type Data = (HashMap<NodeIndex,usize>,
              HashMap<String,NodeIndex>,
-            Graph<usize, usize, Undirected>);
+            Graph<String, usize, Undirected>);
 
 // Format is 
 // Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
@@ -24,10 +24,12 @@ fn input_generator_inner(input: &str) -> Result<Data> {
     let mut rate_map = HashMap::new();
     // map of valve names to graph indices
     let mut valve_name_map = HashMap::new();
+    // map of graph indices to valve names, for convenience
+    let mut valve_index_map = HashMap::new();
     // the actual graph
-    let mut graph: Graph<usize,usize,Undirected> = Graph::new_undirected();
-    // name of one node to another node
-    let mut edges = vec![];
+    let mut graph: Graph<String,usize,Undirected> = Graph::new_undirected();
+
+    let mut edges = HashMap::new();
     for line in input.lines() {
         let (valve_str, tunnel_str) = line.split_once("; ").unwrap();
 
@@ -37,9 +39,10 @@ fn input_generator_inner(input: &str) -> Result<Data> {
         let valve_str = valve_str.to_string();
         let flow_rate:usize = flow_rate_str.parse()?;
 
-        let node_index = graph.add_node(flow_rate);
+        let node_index = graph.add_node(valve_str.clone());
         rate_map.insert(node_index, flow_rate);
         valve_name_map.insert(valve_str.clone(), node_index);
+        valve_index_map.insert(node_index, valve_str.clone());
 
         // tunnels lead to valves DD, II, BB
         // grammar: there might only be one tunnel
@@ -47,19 +50,45 @@ fn input_generator_inner(input: &str) -> Result<Data> {
             Some(many_tunnel) => many_tunnel,
             None => tunnel_str.strip_prefix("tunnel leads to valve ").unwrap(),
         };
+        // put the names of the destinations since they might not have assigned indices yet
         let connected: Vec<&str> = tunnel_str.split(", ").collect();
+        edges.insert(node_index, connected);
 
-        for destination_str in connected {
-            edges.push((valve_str.clone(), destination_str.to_string()));
-        }
     }
     // all the nodes are added: now add the edges
-    for (a_name, b_name) in edges{
-        let &a = valve_name_map.get(&a_name).unwrap();
-        let &b = valve_name_map.get(&b_name).unwrap();
-        // all edges have weight 1
-        // it's an undirected graph so use update_edge so we don't have duplicates
-        graph.update_edge(a, b, 1);
+    for source in graph.node_indices() {
+        // Valves with flow rate 0 can be replaced by longer tunnels
+        // except AA because that's where we start
+        let &source_rate = rate_map.get(&source).unwrap();
+        let source_name = valve_index_map.get(&source).unwrap();
+        if source_rate == 0 && source_name != "AA" {
+            continue;
+        }
+
+        let destinations = edges.get(&source).unwrap();
+        let mut destinations: Vec<(String, Vec<NodeIndex>)> = destinations
+            .iter().map(|&d| (d.to_string(), vec![source])).collect();
+
+        while !destinations.is_empty() {
+            let (destination_name, predecessors) = destinations.pop().unwrap();
+            let &destination = valve_name_map.get(&destination_name).unwrap();
+            let &dest_rate = rate_map.get(&destination).unwrap();
+            if dest_rate != 0 || destination_name == "AA" {
+                // a regular tunnel from source to dest
+                graph.update_edge(source, destination, predecessors.len());
+            }
+            else {
+                // treat this valve as part of a longer tunnel
+                let mut further_predecessors = predecessors.clone();
+                further_predecessors.push(destination);
+                for &further_name in edges.get(&destination).unwrap() {
+                    let &further = valve_name_map.get(further_name).unwrap();
+                    if !predecessors.contains(&further) {
+                        destinations.push((further_name.to_string(), further_predecessors.clone()))
+                    }
+                }
+            }
+        }
     }
 
     Ok((rate_map, valve_name_map, graph))
@@ -67,15 +96,13 @@ fn input_generator_inner(input: &str) -> Result<Data> {
 
 // brute force DFS
 // returns the highest pressure it saw in 30 steps
-fn dfs(g: &Graph<usize, usize, Undirected>, 
-    cur_node:NodeIndex, cur_path:&mut Vec<NodeIndex>, cur_step:usize,
+fn dfs(g: &Graph<String, usize, Undirected>, 
+    cur_node:NodeIndex, cur_path:&mut Vec<NodeIndex>, cur_step:usize, max_steps:usize,
     indices_to_rates:&HashMap<NodeIndex,usize>, total_rate:usize, cumulative_pressure:usize) // this feels bad ._.
     -> usize
 {
     // want total pressure by minute 30
-    let maxlen = 30;
-    if cur_step >= maxlen {
-        //dbg!(cumulative_pressure);
+    if cur_step >= max_steps {
         return cumulative_pressure;
     }
 
@@ -84,9 +111,8 @@ fn dfs(g: &Graph<usize, usize, Undirected>,
     // if we've hit max rates, no point in continuing down
     // return with the pressure that would accumulate in the remaining time
     if total_rate == max_rate {
-        let remaining = maxlen - cur_step;
+        let remaining = max_steps - cur_step;
         let updated_pressure = cumulative_pressure + (max_rate * remaining);
-        //dbg!(remaining, cumulative_pressure, updated_pressure);
         return updated_pressure;
     }
 
@@ -98,7 +124,7 @@ fn dfs(g: &Graph<usize, usize, Undirected>,
         let &my_rate = indices_to_rates.get(&cur_node).unwrap();
         cur_path.push(cur_node);
         max_seen = dfs(g, cur_node, cur_path, cur_step+1, 
-            indices_to_rates, 
+            max_steps, indices_to_rates, 
             total_rate + my_rate, total_rate + cumulative_pressure);
     }
     // move to neighbor
@@ -112,11 +138,22 @@ fn dfs(g: &Graph<usize, usize, Undirected>,
             None
         };
         for neighbor in g.neighbors(cur_node)
-        {
+        {   
             if Some(neighbor) != prev_node || g.neighbors(cur_node).count() == 1 {
-                let p = dfs(g, neighbor, cur_path, cur_step + 1,
-                    indices_to_rates, 
-                    total_rate, total_rate + cumulative_pressure, );
+                // there should only be one edge connecting cur_node to neigbor
+                let edge = g.edges_connecting(cur_node, neighbor).next().unwrap();
+                let mut updated_pressure = cumulative_pressure;
+                let mut updated_step = cur_step;
+                for _ in 0..*edge.weight() {
+                    updated_pressure += total_rate;
+                    updated_step += 1;
+                    if cur_step >= max_steps {
+                        return updated_pressure;
+                    }
+                }
+                let p = dfs(g, neighbor, cur_path, updated_step,
+                    max_steps, indices_to_rates, 
+                    total_rate, updated_pressure );
                 max_seen = max(max_seen, p);
             }
         }
@@ -138,13 +175,13 @@ pub fn solve_part1(input: &Data) -> usize {
 }
 fn solve_part1_inner(input: &Data) -> usize {
     let (rate_map, valve_name_map, tunnels) = input.clone();
-
+    dbg!(&tunnels);
     //dbg!(&rate_map);
 
     // start at valve AA
     let &cur_node = valve_name_map.get("AA").unwrap();
     let mut cur_path = vec![];
-    dfs(&tunnels, cur_node, &mut cur_path, 0, 
+    dfs(&tunnels, cur_node, &mut cur_path, 0, 30, 
         &rate_map, 0, 0)
 }
 
